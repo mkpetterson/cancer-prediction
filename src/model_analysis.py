047402a57a365d4461939e4a5b494025d8ee5133
mkpetterson <maureen.petterson@gmail.com>
2020-06-05 16:51:15 -0700

    updated images

--- src/model_analysis.py
+++ src/model_analysis.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import roc_curve, roc_auc_score, confusion_matrix, plot_confusion_matrix, plot_roc_curve
 
 
 
@@ -29,4 +32,28 @@ def make_dataframe(name, filenames, y_true, y_pred, y_proba):
     
     return None
 
+
+def make_confusion(name, y_true, y_pred, y_proba):
+    
+    # Make confusion plot
+    fig, ax = plt.subplots()
+    cf_matrix = confusion_matrix(y_true, y_pred)
+    sns_plot = sns.heatmap(cf_matrix, annot=True, fmt='5.0f', cmap='Blues')
+    fig = sns_plot.get_figure()
+    plt.title(f'Confusion Matrix: {name}')
+    fig.savefig(f'cf_{name}.png', dpi=350) 
+    plt.close()   
+
+def plot_roc(ax, label, y_true, y_proba):
+    
+    auc = roc_auc_score(y_true, y_proba[:,1])
+    fpr, tpr, thresholds = roc_curve(y_true, y_proba[:,1])    
+    
+    ax.plot(fpr, tpr, label=f'{label} AUC: {auc:2.2f}')
+    ax.legend(loc='lower right')
+    ax.set_xlabel("False Positive Rate")
+    ax.set_ylabel("True Positive Rate")
+    ax.set_title("ROC Curve")   
+
+    return ax
 
